4ce46314ec856042ef25f514784c5761969cd508
LuannLopes <luanlopesofc@gmail.com>
2024-04-02 10:32:35 -0300

    feat: implement outlet routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from '../containers/Home'
+import DefaultLayout from '../layout/DefaultLayout'
 import Movies from './../containers/Movies'
 import Series from './../containers/Series'
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/filmes" element={<Movies />} />
-      <Route path="/series" element={<Series />} />
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/filmes" element={<Movies />} />
+        <Route path="/series" element={<Series />} />
+      </Route>
     </Routes>
   )
 }
