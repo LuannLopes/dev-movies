ee4ea3e3f3ebb58471cece99ae861052ca943fd2
LuannLopes <luanlopesofc@gmail.com>
2024-04-05 08:56:41 -0300

    feat: access data from api

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,15 +1,33 @@
-// import api from '../../services/api'
+import { useEffect, useState } from 'react'
+
+import api from '../../services/api'
+import { Background } from './styles'
 
 function Home() {
-  async function getMovies() {
-    // const data = await api.get('/movie/popular')
-  }
-  getMovies()
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    async function getMovies() {
+      const {
+        data: { results }
+      } = await api.get('/movie/popular')
+
+      setMovie(results[1])
+    }
+    getMovies()
+  }, [])
 
   return (
-    <div>
-      <h1>Hello, Home.</h1>
-    </div>
+    <>
+      {movie && (
+        <Background
+          $img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        >
+          <h1>{movie.title}</h1>
+          <p>{movie.overview}</p>
+        </Background>
+      )}
+    </>
   )
 }
 
