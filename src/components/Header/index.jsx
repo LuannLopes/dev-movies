11c7f5c5c43248a18ad960839de8c97be842c270
LuannLopes <luanlopesofc@gmail.com>
2024-04-07 09:19:37 -0300

    feat: implement header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
 import Logo from '../../assets/logo.png'
+import { Container, Li, Menu } from './styles'
 
 function Header() {
+  const [changeBackground, setChangeBackground] = useState()
+  const { pathname } = useLocation()
+
+  window.onscroll = () => {
+    if (!changeBackground && window.scrollY > 150) {
+      setChangeBackground(true)
+    }
+    if (changeBackground && window.scrollY <= 150) {
+      setChangeBackground(false)
+    }
+  }
+
   return (
-    <div>
-      <img src={Logo} alt="logo-dev-movies" style={{ width: 500 }} />
-    </div>
+    <Container $changeBackground={changeBackground}>
+      <img src={Logo} alt="logo-dev-movies" />
+      <Menu>
+        <Li $isActive={pathname === '/'}>
+          <Link to="/">Home</Link>
+        </Li>
+        <Li $isActive={pathname.includes('filmes')}>
+          <Link to="/filmes">Filmes</Link>
+        </Li>
+        <Li $isActive={pathname.includes('series')}>
+          <Link to="/series">Séries</Link>
+        </Li>
+      </Menu>
+    </Container>
   )
 }
 
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -51,7 +51,6 @@ function Home() {
         data: { results }
       } = await api.get('/person/popular')
 
-      console.log(results)
       setPopularPeople(results)
     }
 
